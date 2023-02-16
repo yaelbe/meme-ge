1fd05f69696e88711aed650552dfc08ff8297418
Yael B <yael@any.do>
2023-02-16 09:04:40 +0200

    add editor ui

--- SITE/js/canvas.controller.js
+++ SITE/js/canvas.controller.js
@@ -1,5 +1,5 @@
 'use strict'
-
+var gLines = []
 function showEditor() {
   addResizeListeners()
   let image = new Image()
@@ -31,6 +31,41 @@ function renderCanvas() {
   gElCanvas.height = imageRatio < 1 ? gElCanvas.width * imageRatio : gElCanvas.width / imageRatio
 
   gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+
+  gCtx.fillStyle = 'white'
+  gCtx.font = 'bold 18px Arial'
+  gCtx.textAlign = 'center'
+  const lineHeight = gCtx.measureText('M').width + 20
+  let centerX = gElCanvas.width / 2
+  let topY = lineHeight
+
+  getLines().forEach((line, index) => {
+    if (line.text.length !== 0) {
+      let y = topY + lineHeight * index
+      let width = gCtx.measureText(line.text).width
+      gCtx.fillText(line.text, centerX, y)
+      gCtx.setLineDash([6])
+      gCtx.strokeStyle = 'white'
+      gCtx.strokeRect(centerX - width / 2, y - lineHeight / 1.5, width, lineHeight)
+    }
+  })
+}
+
+function onTextInput(text) {
+  console.log(text)
+  let line = getCurrentLine()
+  if (!line) {
+    line = createLine()
+  }
+  line.text = text
+  renderCanvas()
+}
+
+function onTypeEnd(ev, el) {
+  if (ev.keyCode === 13) {
+    el.value = ''
+    createLine()
+  }
 }
 
 function removeResizeListener() {
--- SITE/js/services/canvas.service.js
+++ SITE/js/services/canvas.service.js
@@ -1,1 +1,16 @@
 'use strict'
+var gLines = []
+function getLines() {
+  return gLines
+}
+function getCurrentLine() {
+  return gLines.at(-1)
+}
+function createLine() {
+  let line = {
+    text: '',
+    position: {},
+  }
+  gLines.push(line)
+  return line
+}
